08facfe376b92d1880738abf4c541c27260ba5f9
Noah <nbryfogle@gmail.com>
2022-09-26 13:24:10 -0400

    Update things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,29 @@
 <script>
     import { AppBar, Button, Icon, MaterialApp } from 'svelte-materialify';
     import { goto } from '$app/navigation';
-    import { mdiHome, mdiNewspaper, mdiCart } from '@mdi/js'
+    import { mdiHome, mdiNewspaper, mdiCart, mdiBrightness6 } from '@mdi/js'
+
+    let theme = "dark";
 </script>
 
-<MaterialApp theme="dark">
+<MaterialApp theme={theme}>
 <AppBar class="pa-1">
-    <div slot="icon">
-        <Button on:click={() => goto("/")}>
-            <Icon path={mdiHome} />
+        <div slot="icon">
+            <Button on:click={() => goto("/")}>
+                <Icon path={mdiHome} />
+            </Button>
+        </div>
+        <span slot="title">CCTI 3D Printing</span>
+        <div style="flex-grow:1" />
+        <Button class="mr-3" on:click={() => goto("/order")}>
+            <Icon path={mdiCart} />
         </Button>
-    </div>
-    <span slot="title">CCTI 3D Printing</span>
-    <div style="flex-grow:1" />
-    <Button class="mr-3" on:click={() => goto("/order")}>
-        <Icon path={mdiCart} />
-    </Button>
-    <!-- <Button class="mr-3" on:click={() => goto("/news")}>
-        <Icon path={mdiNewspaper} />
-    </Button> -->
+        <Button on:click={() => {if (theme === 'light') { theme = 'dark' } else {theme='light'}}} class="mr-3"> 
+            <Icon path={mdiBrightness6} />
+        </Button>
+        <!-- <Button class="mr-3" on:click={() => goto("/news")}>
+            <Icon path={mdiNewspaper} />
+        </Button> -->
 </AppBar>
 <slot></slot>
 </MaterialApp>
