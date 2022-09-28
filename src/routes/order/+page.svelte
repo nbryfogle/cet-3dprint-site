1ecd04afdb2323eebc0dfe82cde7005fe57f0ff5
nbryfogle <nbryfogle@gmail.com>
2022-09-27 22:45:54 -0400

    Revert "Increase email character limit"
    This reverts commit f32c05eba469e77cee2f82fa0d33d83fa87deb13.

--- src/routes/order/+page.svelte
+++ src/routes/order/+page.svelte
@@ -38,7 +38,7 @@
 
     const emailRules = [
         (v) => !!v || 'Required',
-        (v) => v.length <= 50 || 'Max 25 characters',
+        (v) => v.length <= 25 || 'Max 25 characters',
         (v) => {
         const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return pattern.test(v) || 'Invalid e-mail.';
